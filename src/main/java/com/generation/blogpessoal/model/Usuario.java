ec504bb969159d2cb6ecea6a5ca3b7e359672f9c
Thiago Tasseli <thiagotasseli@outlook.com.br>
2025-05-28 09:42:30 -0300

    :wrench: alteração no FetchType.LAZY na Classe Usuario na Model

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -38,7 +38,7 @@ public class Usuario {
 	@Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres")
 	private String foto;
 
-	@OneToMany(fetch = FetchType.EAGER, mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
 
